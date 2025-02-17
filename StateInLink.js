35c4db323b88a4aab3560e06cfb223dde32bd087
MD Nadim Rijvi Shakib <84374961+nrshakib@users.noreply.github.com>
2025-02-17 14:29:07 +0600

    Update StateInLink.js

--- StateInLink.js
+++ StateInLink.js
@@ -1,8 +1,22 @@
 // This is how Data can be sent via Link of React Route Dom using State prop  
-
-
 <Link
-   to={`/edit-court/${record._id}`}
+   to={`/product-details/${record._id}`}
    state={record}>
    <Button icon={<EditOutlined />} style={{ marginRight: 8 }} />
 </Link>
+
+// This is how the data should be received
+import { useLocation } from "react-router-dom";
+
+const ProductDetails = () => {
+  const location = useLocation();
+  const details = location.state?.record || "No data received";
+
+  return (
+    <div>
+      <h1>ProductDetails Page</h1>
+      <p>{details}</p>
+    </div>
+  );
+};
+export default ProductDetails;
